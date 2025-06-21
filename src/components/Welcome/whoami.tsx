8a249597e970953f853471c7adfc390515a503da
Akhila Ananth <akhilasananth@gmail.com>
2025-06-21 21:19:43 +0200

    Home page

--- src/components/Welcome/whoami.tsx
+++ src/components/Welcome/whoami.tsx
@@ -8,7 +8,8 @@ export default function WhoAmI() {
         'Creative',
         'Traveller',
         'Storyteller',
-        'Human'
+        'Runner',
+        'Human',
     ];
 
     useEffect(() => {
